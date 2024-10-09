8d85d541e022b1616983bb1ed5821ceda3ae8ce6
Reedus0 <lazsasha2006@gmail.com>
2024-10-09 16:46:19 +0300

    hosts

--- Dockerfile
+++
@@ -1,5 +0,0 @@
-FROM python
-WORKDIR /app
-COPY . . 
-RUN pip install -r requirements.txt
-CMD ["python3", "src/main.py"]
--- db/replication.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sleep 5
-pg_ctl stop
-rm -rf /var/lib/postgresql/data/*
-pg_basebackup --pgdata=/var/lib/postgresql/data -R --slot=replication_slot --host=db_host --port=5432
-pg_ctl start
