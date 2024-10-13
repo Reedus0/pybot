347fc22fcf7846e5b0d6cbf8c72b1ae505b0df93
Reedus0 <lazsasha2006@gmail.com>
2024-10-13 10:35:55 +0300

    logs

--- src/logger.py
+++ src/logger.py
@@ -2,11 +2,11 @@ import logging
 import os
 
 def init_logging():
-    log_path = os.getcwd() + os.getenv("LOG_PATH")
-    log_name = os.getenv("LOG_FILE_NAME")
+    log_path = "/logs/"
+    log_name = "bot.log"
 
     if not os.path.exists(log_path):
-        os.makedirs(log_path)
+        os.makedirs(log_path, 777)
 
     logging.basicConfig(
         filename=log_path + log_name, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
